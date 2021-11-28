181f8323dd96963ad6d73c0a03bcffb54a01f3e6
By-Biska <varakinkirill3@gmail.com>
2021-11-28 12:15:11 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.home-section__slider').slick ({
+        infinite: true,
+        fade: true,
+        prevArrow: '<button class="home-section__slider-btn home-section__slider-btnprev "><img src="../images/arrow-left.svg"</button>',
+        nextArrow: '<button class="home-section__slider-btn home-section__slider-btnnext "><img src="../images/arrow-right.svg"</button>'
+    })
+})
