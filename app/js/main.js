79d1348a79886f71a089c1274dc269bbffec43bb
By-Biska <varakinkirill3@gmail.com>
2021-12-02 22:44:25 +0300

    html

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,10 @@ $(function () {
         prevArrow: '<button class="home-section__slider-btn home-section__slider-btnprev "><img src="../images/arrow-left.svg"</button>',
         nextArrow: '<button class="home-section__slider-btn home-section__slider-btnnext "><img src="../images/arrow-right.svg"</button>'
     })
+    $('.why-section__slider').slick ({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    })
 })
